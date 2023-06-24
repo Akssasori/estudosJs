2f74171312b5c5b5cc80d2f666b10a4a2441d3fa
Akssasori <diniz3003@gmail.com>
2023-06-24 14:29:08 -0300

    try catch promisse

--- promises.js
+++ promises.js
@@ -1,7 +1,41 @@
+function aguardarSetTimeOut() {
+  //Sempre passo dois parametros(resolve, reject)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // console.log("Segunda mensagem");
+      resolve("Segunda mensagem");
+    }, 1000);
+  });
+}
+
+async function executar() {
+
+  try {
+
+    console.log("Primeira mensagem");
+
+    const segundaMensagem = await aguardarSetTimeOut();
+    console.log(segundaMensagem);
+  
+    console.log("Terceira mensagem");
+    
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+executar();
+
+//Then() vai esperar dentro de um escopo especifico o escope dele, se for no escopo de função ai usar async await
+
 console.log("Primeira mensagem");
 
-setTimeout(() => {
-  console.log("Segunda mensagem");
-}, 1000);
+aguardarSetTimeOut().then(result => {
+  console.log("Inicio");
+  console.log(result);
+  console.log("Fim");
+})
+// console.log(segundaMensagem);
 
 console.log("Terceira mensagem");
