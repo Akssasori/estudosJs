f94bbb1574bbb660085fa9c024ce0a46ab2cb271
Akssasori <diniz3003@gmail.com>
2023-07-01 08:54:54 -0300

    criando rota post

--- criadoServer/index.js
+++ criadoServer/index.js
@@ -1,39 +1,31 @@
 const http = require("http");
+const { randomUUID } = require("crypto");
+
+let users = [];
 
 const server = http.createServer((request, response) => {
-  console.log(request.url);
 
-  if (request.url === "/minha-primeira-rota") {
-    const result = { message: "Minha primeira rota!" };
+  // Cadastro de usuário
 
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "application/json");
-    // response.statusMessage("Usuário salco com sucesso");
-    //   response.writeHead(201, {
-    //       "Content-Type": "application/json",
-    //     })
+  const METHOD = request.method;
 
-    // response.write(JSON.stringify(result));
-    return response.end(JSON.stringify(result));
+  if (METHOD === "POST") {
+
+    request.on("data", (data) => {
+
+      const body = JSON.parse(data);
+      
+      const user = {
+        ...body,
+        id: randomUUID(),
+      }
+      
+      users.push(user);
+      console.log(body);
+      
+      return response.end(JSON.stringify(users));
+    });
   }
-
-  if (request.url === "/minha-segunda-rota") {
-
-    const result = { message: "Minha segunda rota!" };
-
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "application/json");
-    return response.end(JSON.stringify(result));
-
-  }
-
-  const result = { message: "Qualquer outra rota!" };
-
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "application/json");
-  response.write(JSON.stringify(result));
-  response.end();
-  
 });
 
 server.listen(3000, () => console.log("Servidor está rodando"));
